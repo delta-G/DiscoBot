fbb5a42e55e2311c78d8f0fadf1e0d2d7d4167b0
david <david@static-disco.fak>
2017-12-29 23:34:32 -0600

    Starting Out

---
+++ DIscoBotGUI.py
@@ -0,0 +1,19 @@
+from Tkinter import *
+
+
+class DiscoBotGUI:
+    
+    def __init__(self, aParent):
+        
+        self.parent = aParent
+        self.mainFrame = Frame(self.parent)
+        
+        
+        
+        
+        return
+    
+    
+    
+
+
